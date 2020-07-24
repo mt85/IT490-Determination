5df1fa8ddc8933c0f8c45bbbf966e3fc63b3abab
sterling-c <clark_sterling@aol.com>
2020-07-24 10:04:42 -0400

    Added necessary files for APP, API, and DB to connect to MQ.

--- API/MQClient.php
+++ API/MQClient.php
@@ -1,1 +1,19 @@
 <?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQlib.inc');
+
+function createClientForDb($request){
+    $client = new rabbitMQClient("rabbitMQ_db.ini", "testServer");
+
+    if(isset($argv[1])){
+        $msg = $argv[1];
+    }
+    else{
+        $msg = "client";
+    }
+
+    $response = $client->send_request($request);
+    return $response;
+}
+?>
